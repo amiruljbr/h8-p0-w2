16c79ad0ed1f1bc0d6c0b47937987c361807553b
Bakti Amirul Jabar <62320698+amiruljbr@users.noreply.github.com>
2020-04-01 13:59:07 +0700

    create fungtion dataHandling

--- week3/exercises3.js
+++ week3/exercises3.js
@@ -1,3 +1,24 @@
+function dataHandling(arr){
+	var strResult='';
+	for (var i=0; i < arr.length; i++){
+		for (var j=0; j < arr[i].length; j++){
+			if (j==0){
+				strResult += 'Nomor ID: ' + arr[i][j] + '\n';
+			} else if (j==1) {
+				strResult += 'Nama Lengkap: ' + arr[i][j] + '\n';
+			} else if (j==2) {
+				strResult += `TTL: ${arr[i][j]} ${arr[i][j+1]}
+`;
+			} else if (j==4 && i!=arr.length-1) {
+				strResult += 'Hobi: ' + arr[i][j] + '\n' + '\n';
+			} else if (j==4 && i==arr.length-1) {
+				strResult += 'Hobi: ' + arr[i][j];
+			}
+		}
+	}
+	return strResult;
+}
+
 var input = [
                 ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
                 ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
@@ -5,19 +26,4 @@ var input = [
                 ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
             ];
 
-for (var i=0; i < input.length; i++){
-	for (var j=0; j < input[i].length; j++){
-		if (j==0){
-			console.log('Nomor ID: ' + input[i][j]);
-		} else if (j==1) {
-			console.log('Nama Lengkap: ' + input[i][j]);
-		} else if (j==2) {
-			console.log(`TTL:  ${input[i][j]} ${input[i][j+1]}`);
-		} else if (j==4 && i!=input.length-1) {
-			console.log('Hobi: ' + input[i][j]);
-			console.log('');
-		} else if (j==4 && i==input.length-1) {
-			console.log('Hobi: ' + input[i][j]);
-		}
-	}
-}
+console.log (dataHandling(input));
