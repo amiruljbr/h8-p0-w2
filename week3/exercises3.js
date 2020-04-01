d65f428263bc31ee81b31010c130d88e00a1e3c6
Bakti Amirul Jabar <62320698+amiruljbr@users.noreply.github.com>
2020-04-01 13:19:14 +0700

    tambah semicolon

--- week3/exercises3.js
+++ week3/exercises3.js
@@ -3,7 +3,7 @@ var input = [
                 ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
                 ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
                 ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
-            ]
+            ];
 
 for (var i=0; i < input.length; i++){
 	for (var j=0; j < input[i].length; j++){
