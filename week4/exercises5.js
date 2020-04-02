4fb15344247eae5223a988dd973a92caca83f4a8
Bakti Amirul Jabar <62320698+amiruljbr@users.noreply.github.com>
2020-04-02 17:52:55 +0700

    add new file

--- week4/exercises5.js
+++ week4/exercises5.js
@@ -1,22 +1,19 @@
-function ubahHuruf(kata) {
+function palindrome(kata) {
   // you can only write your code here!
-	var abjad = 'abcdefghijklmnopqrstuvwxyza';
-	var hasilkata = '';
-	for (var i=0; i< kata.length; i++){
-		for (var j=0; j< abjad.length-1; j++){
-			if (kata[i] == abjad[j]){
-				hasilkata = hasilkata + abjad[j+1];
-			}
-		}
-	}
-	return hasilkata;
+  var balikata='';
+  for (var i=0; i< kata.length; i++){
+	  balikata = kata[i] + balikata;
+  }
+  if (balikata == kata){
+	  return true;
+  } else {
+	  return false;
+  }
 }
 
 // TEST CASES
-console.log(ubahHuruf('wow')); // xpx
-console.log(ubahHuruf('developer')); // efwfmpqfs
-console.log(ubahHuruf('javascript')); // kbwbtdsjqu
-console.log(ubahHuruf('keren')); // lfsfo
-console.log(ubahHuruf('semangat')); // tfnbohbu
-console.log(ubahHuruf('zebra')); // afcsb
-console.log(ubahHuruf('zabcdefghijklmnopqrstuvwxyzabcde')); // abcdefghijklmnopqrstuvwxyzabcdef
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
