56c71d1cca550834fe04c7029479cf2fe9ec6f82
amiruljbr <you@example.com>
2020-04-02 22:05:21 +0700

    penambahan validasi

--- week4/exercises10_changeme.js
+++ week4/exercises10_changeme.js
@@ -1,9 +1,16 @@
 function changeMe(arr) {
 	for (var i=0; i< arr.length; i++){
+		var tahunsekarang = 2020; //karena saat ini tahun 2020
+		var umur;
+		if (tahunsekarang <= arr[i][3] || arr[i][3] == undefined || isNaN(arr[i][3]) == true){
+			umur = 'Invalid Birth Year';
+		} else {
+			umur = tahunsekarang - arr[i][3];
+		}
 		var bio = { firstName: arr[i][0],
 						lastName: arr[i][1],
 						gender: arr[i][2],
-						age: 2019 - arr[i][3] 
+						age: umur
 						};
 		console.log(i+1 + '. ' + bio.firstName + ' ' + bio.lastName + ':');
 		console.log(bio);
@@ -12,7 +19,7 @@ function changeMe(arr) {
 
 // TEST CASES
 	
-changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
+//changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male', 2090]]); 
 // 1. Christ Evans:
 // { firstName: 'Christ',
 //   lastName: 'Evans',
@@ -23,3 +30,4 @@ changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]);
 //   lastName: 'Downey',
 //   gender: 'Male',
 //   age: 'Invalid Birth Year' }
+changeMe(['a']); // ""
